0f13b3363a16317636c5bf58b21065e51453efbf
Lucas Moura <lucas.moura128@gmail.com>
2017-09-22 17:35:22 -0300

    Create Glove model

--- research/glove/glove.py
+++ research/glove/glove.py
@@ -1,112 +1,167 @@
 import os
+import sys
 
 import tensorflow as tf
+from word_embedding import WordEmbedding, FLAGS, Options
+
+from six.moves import xrange  # pylint: disable=redefined-builtin
 
 glove = tf.load_op_library(
     os.path.join(os.path.dirname(os.path.realpath(__file__)), 'glove_ops.so'))
 
 
-"""
-The flags and Config class are based on the official word2vec model on
-tensorflow
+class GloVe(WordEmbedding):
 
-https://github.com/tensorflow/models/blob/master/tutorials/embedding/word2vec.py
-"""
+    def forward(self, inputs, labels, **kwargs):
+        opts = self._options
+        init_width = 1.0
 
-flags = tf.app.flags
+        self.input_embeddings = tf.Variable(
+            tf.random_uniform(
+                [opts.vocab_size, opts.emb_dim], -init_width, init_width),
+            name="input_embeddings")
 
-flags.DEFINE_string('save_path', None, 'Directory to write the model and '
-                    'training summaries.')
-flags.DEFINE_string('train_data', None, 'Training text file. '
-                    'E.g., unzipped file http://mattmahoney.net/dc/text8.zip.')
-flags.DEFINE_string(
-    'eval_data', None, 'File consisting of analogies of four tokens.'
-    'embedding 2 - embedding 1 + embedding 3 should be close to embedding 4.')
-flags.DEFINE_integer('embedding_size', 200, 'The embedding dimension size.')
-flags.DEFINE_integer(
-    'epochs_to_train', 15,
-    'Number of epochs to train. Each epoch processes the training data once '
-    'completely.')
-flags.DEFINE_float('learning_rate', 0.2, 'Initial learning rate.')
-flags.DEFINE_integer('batch_size', 16,
-                     'Number of training examples processed per step '
-                     '(size of a minibatch).')
-flags.DEFINE_integer('concurrent_steps', 12,
-                     'The number of concurrent training steps.')
-flags.DEFINE_integer('window_size', 5,
-                     'The number of words to predict to the left and right '
-                     'of the target word.')
-flags.DEFINE_integer('min_count', 5,
-                     'The minimum number of word occurrences for it to be '
-                     'included in the vocabulary.')
-flags.DEFINE_integer('statistics_interval', 5,
-                     'Print statistics every n seconds.')
-flags.DEFINE_integer('summary_interval', 5,
-                     'Save training summary to file every n seconds (rounded '
-                     'up to statistics interval).')
-flags.DEFINE_integer('checkpoint_interval', 600,
-                     'Checkpoint the model (i.e. save the parameters) every n '
-                     'seconds (rounded up to statistics interval).')
+        # Transposed context embeddings
+        self.context_embeddings = tf.Variable(
+            tf.random_uniform(
+                [opts.vocab_size, opts.emb_dim], -init_width, init_width),
+            name="context_embeddings")
 
-FLAGS = flags.FLAGS
+        input_biases = tf.Variable(
+            tf.random_uniform([opts.vocab_size], -init_width, init_width),
+            name="input_biases")
 
+        context_biases = tf.Variable(
+            tf.random_uniform([opts.vocab_size], -init_width, init_width),
+            name="context_biases")
 
-class Config():
-    """Configuration used by our GloVe model."""
+        # Embeddings for examples: [batch_size, emb_dim]
+        input_embedings = tf.nn.embedding_lookup(
+            self.input_embeddings, inputs)
 
-    def __init__(self):
-        # Embedding dimension.
-        self.emb_dim = FLAGS.embedding_size
+        # Embeddings for labels: [batch_size, vocab_size]
+        labels_embeddings = tf.nn.embedding_lookup(
+            self.context_embeddings, labels)
 
-        # Training options.
-        # The training text file.
-        self.train_data = FLAGS.train_data
+        # biases for examples: [batch_size]
+        input_biases = tf.nn.embedding_lookup(
+            input_biases, inputs)
 
-        # The initial learning rate.
-        self.learning_rate = FLAGS.learning_rate
+        # biases for labels: [batch_size]
+        labels_biases = tf.nn.embedding_lookup(
+            context_biases, labels)
 
-        # Number of epochs to train. After these many epochs, the learning
-        # rate decays linearly to zero and the training stops.
-        self.epochs_to_train = FLAGS.epochs_to_train
+        self.global_step = tf.Variable(0, name="global_step")
 
-        # Concurrent training steps.
-        self.concurrent_steps = FLAGS.concurrent_steps
+        return (input_embedings, input_biases,
+                labels_embeddings, labels_biases)
 
-        # Number of examples for one training step.
-        self.batch_size = FLAGS.batch_size
+    def loss(self, **kwargs):
+        ccounts = kwargs['ccounts']
+        inputs_embeddings = kwargs['inputs_embeddings']
+        inputs_biases = kwargs['inputs_biases']
+        labels_embeddings = kwargs['labels_embeddings']
+        labels_biases = kwargs['labels_biases']
+        alpha_value = 0.75
+        x_max = 100
 
-        # Number of words to predict to the left and right of the target word.
-        self.window_size = FLAGS.window_size
+        # Co-ocurrences log
+        log_coocurrences = tf.log(tf.to_float(ccounts))
 
-        # The minimum number of word occurrences for it to be included in the
-        # vocabulary.
-        self.min_count = FLAGS.min_count
+        embedding_product = tf.reduce_sum(
+            tf.multiply(inputs_embeddings, labels_embeddings), 1)
 
-        # How often to print statistics.
-        self.statistics_interval = FLAGS.statistics_interval
+        distance_score = tf.square(
+                tf.add_n([embedding_product,
+                          inputs_biases,
+                          labels_biases,
+                          -log_coocurrences]))
 
-        # How often to write to the summary file (rounds up to the nearest
-        # statistics_interval).
-        self.summary_interval = FLAGS.summary_interval
+        weighting_factor = tf.minimum(
+            1.0,
+            tf.pow(tf.div(ccounts, x_max), alpha_value))
 
-        # How often to write checkpoints (rounds up to the nearest statistics
-        # interval).
-        self.checkpoint_interval = FLAGS.checkpoint_interval
+        loss = tf.reduce_sum(
+            tf.multiply(weighting_factor, distance_score))
 
-        # Where to write out summaries.
-        self.save_path = FLAGS.save_path
-        if not os.path.exists(self.save_path):
-            os.makedirs(self.save_path)
+        return loss
 
-        # Eval options.
-        # The text file for eval.
-        self.eval_data = FLAGS.eval_data
+    def optimize(self, loss):
+        opts = self._options
+        lr = opts.learning_rate
 
+        optimizer = tf.train.AdagradOptimizer(lr)
+        self._lr = lr
+        train = optimizer.minimize(loss,
+                                   global_step=self.global_step,
+                                   gate_gradients=optimizer.GATE_NONE)
+        self._train = train
 
+    def build_graph(self):
+        opts = self._options
+
+        print('Calculating Co-ocurrence matrix...')
+        (words, _, _, words_per_epoch,
+         self._epoch, self._words, examples, labels,
+         ccounts) = glove.glove_model(filename=opts.train_data,
+                                      batch_size=opts.batch_size,
+                                      window_size=opts.window_size,
+                                      min_count=opts.min_count)
+        (opts.vocab_words, opts.words_per_epoch) = self._session.run(
+                 [words, words_per_epoch])
+        opts.vocab_size = len(opts.vocab_words)
+
+        print("Data file: ", opts.train_data)
+        print("Vocab size: ", opts.vocab_size - 1, " + UNK")
+        print("Words per epoch: ", opts.words_per_epoch)
+
+        self._examples = examples
+        self._labels = labels
+        self._ccounts = ccounts
+        self._id2word = opts.vocab_words
+
+        for i, w in enumerate(self._id2word):
+            self._word2id[w] = i
+
+        (inputs_embeddings, inputs_biases,
+         labels_embeddings, labels_biases) = self.forward(examples, labels)
+
+        loss_variables = {"ccounts": ccounts,
+                          "inputs_embeddings": inputs_embeddings,
+                          "inputs_biases": inputs_biases,
+                          "labels_embeddings": labels_embeddings,
+                          "labels_biases": labels_biases}
+        loss = self.loss(**loss_variables)
+
+        self._loss = loss
+        tf.summary.scalar("GloVe loss", loss)
+        self.optimize(loss)
+
+        tf.global_variables_initializer().run()
+        self.saver = tf.train.Saver()
+
+    def set_embeddings(self):
+        self._embeddings = tf.add(self.input_embeddings,
+                                  self.context_embeddings)
 
 
 def main():
-    pass
+    """Train a GloVe model."""
+    if not FLAGS.train_data or not FLAGS.eval_data or not FLAGS.save_path:
+        print("--train_data --eval_data and --save_path must be specified.")
+        sys.exit(1)
+    opts = Options()
+    with tf.Graph().as_default(), tf.Session() as session:
+        with tf.device("/cpu:0"):
+            model = GloVe(opts, session)
+            model.read_analogies()  # Read analogy questions
+        for _ in xrange(opts.epochs_to_train):
+            model.train()  # Process one epoch
+            model.eval()  # Eval analogies.
+            # Perform a final save.
+        model.saver.save(session,
+                         os.path.join(opts.save_path, "model.ckpt"),
+                         global_step=model.global_step)
 
 
 if __name__ == '__main__':
